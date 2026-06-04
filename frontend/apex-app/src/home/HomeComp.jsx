ded46e082771b3a9616271cd9e5c12a7bd269ca1
apexplacements <apex07.placements@gmail.com>
2026-06-04 23:29:41 +0530

    add home component

---
+++ frontend/apex-app/src/home/HomeComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomeComp = () => {
+  return (
+    <div>
+        <h1>HomeComp</h1>
+        <p>Welcome to the Home Component!</p>
+    </div>
+  )
+}
+
+export default HomeComp;
